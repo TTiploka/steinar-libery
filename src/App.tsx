e07ec4250dfdac5d243df9076bfe7c2aa4fc8cfc
TTiploka <hcobasmatos@gmail.com>
2023-10-11 19:37:20 -0400

    Adding Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 // Importo las imagenes
-import book1 from './assets/Alice in Wonderland.jpg'
-import book2 from './assets/Le petit prince.jpg'
-import book3 from './assets/El tonto de chipre.jpg'
-import book4 from './assets/Habitos atonicos.jpg'
-import book5 from './assets/Psicologia del exito.jpg'
-import book6 from './assets/Donde no hay.jpg'
+
 
 import facebook from './assets/icon_f.png'
 import twiter from './assets/icon_t.png'
 import whatsapp from './assets/icon_w.png'
 
-// Errores
-// 1 - Le faltaba estructura al html
-// 2 - Los titulos van dentro de headers ejemplo h1
-// 3 - no limpiaste los estilos
+import Art from './Componentes/art'
 
 export default function App() {
   return (<>
@@ -26,53 +18,12 @@ export default function App() {
     de la pantalla y que estos no se modifiquen y mx-auto es para que 
     siempre tengan el mismo margen de los lados*/}
     <section className='grid md:grid-cols-3 gap-4 text-xl md:text-2xl container mx-auto'>
-      <article className='flex gap-2 text-gray-400 hover:text-blue-600 rounded bg-stone-800 shadow hover:shadow-md hover:shadow-blue-700'>
-        <img className='w-20 h-28 object-cover m-2 border rounded' src={book1} alt="" />
-        <hgroup className='flex flex-col justify-center'>
-          <h1>Alice in Wonderland</h1>
-          <p className='text-lg'>Aventura y fantasia</p>
-        </hgroup>
-      </article>
-
-      <article className='flex gap-2 text-gray-400 hover:text-blue-600 rounded bg-stone-800 shadow hover:shadow-md hover:shadow-blue-700'>
-        <img className='w-20 h-28 object-cover m-2 border rounded' src={book2} alt="" />
-        <hgroup className='flex flex-col justify-center'>
-          <h1>Le pit prince</h1>
-          <p className='text-lg'>Drama infantil</p>
-        </hgroup>
-      </article>
-
-      <article className='flex gap-2 text-gray-400 hover:text-blue-600 rounded bg-stone-800 shadow hover:shadow-md hover:shadow-blue-700'>
-        <img className='w-20 h-28 object-cover m-2 border rounded' src={book3} alt="" />
-        <hgroup className='flex flex-col justify-center'>
-          <h1>El tonto de chipre</h1>
-          <p className='text-lg'>Cuento infantil</p>
-        </hgroup>
-      </article>
-
-      <article className='flex gap-2 text-gray-400 hover:text-blue-600 rounded bg-stone-800 shadow hover:shadow-md hover:shadow-blue-700'>
-        <img className='w-20 h-28 object-cover m-2 border rounded' src={book4} alt="" />
-        <hgroup className='flex flex-col justify-center'>
-          <h1>Habitos Atomicos</h1>
-          <p className='text-lg'>Superacion personal</p>
-        </hgroup>
-      </article>
-
-      <article className='flex gap-2 text-gray-400 hover:text-blue-600 rounded bg-stone-800 shadow hover:shadow-md hover:shadow-blue-700'>
-        <img className='w-20 h-28 object-cover m-2 border rounded' src={book5} alt="" />
-        <hgroup className='flex flex-col justify-center'>
-          <h1>Psicologia del exito</h1>
-          <p className='text-lg'>Superacion Personal</p>
-        </hgroup>
-      </article>
-
-      <article className='flex gap-2 text-gray-400 hover:text-blue-600 rounded bg-stone-800 shadow hover:shadow-md hover:shadow-blue-700'>
-        <img className='w-20 h-28 object-cover m-2 border rounded' src={book6} alt="" />
-        <hgroup className='flex flex-col justify-center'>
-          <h1>Donde no hay doctor</h1>
-          <p className='text-lg'>Medicina casera</p>
-        </hgroup>
-      </article>
+       <Art num={0}></Art>
+       <Art num={1}></Art>
+       <Art num={2}></Art>
+       <Art num={3}></Art>
+       <Art num={4}></Art>
+       <Art num={5}></Art>
     </section>
 
     <footer className='w-full border-sky-500 border-t-2 p-2 flex text-small italic text-gray-600 justify-center mt-20'>
--- src/Componentes/Art.tsx
+++ src/Componentes/Art.tsx
@@ -1,10 +1,12 @@
-
-export default function Art() {
-   
+import { books } from "./books"
+export default function Art(props:{num:number}) {
     return <>
-        { }
-        <button >
-            
-        </button>
+        <article className='flex gap-2 text-gray-400 hover:text-blue-600 rounded bg-stone-800 shadow hover:shadow-md hover:shadow-blue-700'>
+        <img className='w-20 h-28 object-cover m-2 border rounded' src={books[props.num].src} alt="" />
+        <hgroup className='flex flex-col justify-center'>
+          <h1>{books[props.num].name}</h1>
+          <p className='text-lg'>{books[props.num].genere}</p>
+        </hgroup>
+        </article>
     </>
 }
