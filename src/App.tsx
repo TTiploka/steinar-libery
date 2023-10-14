a72f68242834080155b093a4e7045cef63fe4cb8
Dricc-new <diegoramoncc17@gmail.com>
2023-10-14 19:15:51 -0400

    añadiendo el router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
 // Importo las imagenes
 
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
-import facebook from './assets/icon_f.png'
-import twiter from './assets/icon_t.png'
-import whatsapp from './assets/icon_w.png'
 
-import Art from './Componentes/Art'
+// import facebook from './assets/icon_f.png'
+// import twiter from './assets/icon_t.png'
+// import whatsapp from './assets/icon_w.png'
 
-import books from './assets/books.json'
+// import Art from './Componentes/Art'
+
+// import books from './assets/books.json'
+
+// export default function App() {
+//   return (<>
+//     <section className='text-white py-10 flex flex-col items-center'>
+//       <h1 className="text-6xl font-bold underline">WELCOME!</h1>
+//       <input className='text-3xl w-80 px-4 h-14 m-8 border rounded-md bg-stone-900' type="text" placeholder='Busca un libro' />
+//     </section>
+
+//     {/*container es una clase para contener los elementos segun la escala 
+//     de la pantalla y que estos no se modifiquen y mx-auto es para que 
+//     siempre tengan el mismo margen de los lados*/}
+//     <section className='grid md:grid-cols-3 gap-4 text-xl md:text-2xl container mx-auto'>
+//       {books.map((item) => <Art name={item.name} genere={item.genere} image={item.image} />)}
+//     </section>
+
+//     <footer className='w-full border-sky-500 border-t-2 p-2 flex text-small italic text-gray-600 justify-center mt-20'>
+//       <h3>Contactenos</h3>
+//       <img className='w-6 h-6 rounded-lg ml-10' src={facebook}></img>
+//       <img className='w-6 h-6 rounded-lg ml-3' src={twiter}></img>
+//       <img className='w-6 h-6 rounded-lg ml-3' src={whatsapp}></img>
+//     </footer>
+//   </>
+//   )
+
+// }
 
 export default function App() {
-  return (<>
-    <section className='text-white py-10 flex flex-col items-center'>
-      <h1 className="text-6xl font-bold underline">WELCOME!</h1>
-      <input className='text-3xl w-80 px-4 h-14 m-8 border rounded-md bg-stone-900' type="text" placeholder='Busca un libro' />
-    </section>
-
-    {/*container es una clase para contener los elementos segun la escala 
-    de la pantalla y que estos no se modifiquen y mx-auto es para que 
-    siempre tengan el mismo margen de los lados*/}
-    <section className='grid md:grid-cols-3 gap-4 text-xl md:text-2xl container mx-auto'>
-      {books.map((item) => <Art name={item.name} genere={item.genere} image={item.image} />)}
-    </section>
-
-    <footer className='w-full border-sky-500 border-t-2 p-2 flex text-small italic text-gray-600 justify-center mt-20'>
-      <h3>Contactenos</h3>
-      <img className='w-6 h-6 rounded-lg ml-10' src={facebook}></img>
-      <img className='w-6 h-6 rounded-lg ml-3' src={twiter}></img>
-      <img className='w-6 h-6 rounded-lg ml-3' src={whatsapp}></img>
-    </footer>
+  return <>
+    <RouterProvider router={router} />
   </>
-  )
- 
 }
---
+++ src/router/index.tsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    { path: "/", element: <h1 className="text-white">Hello world!</ h1 > },
+]);
+
+export default router
