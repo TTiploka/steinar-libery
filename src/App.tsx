63bb32b66b6699da47d3079d3967957709d4fb6f
Dricc-new <diegoramoncc17@gmail.com>
2023-10-13 22:43:33 -0400

    Corrijiendo componente

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import facebook from './assets/icon_f.png'
 import twiter from './assets/icon_t.png'
 import whatsapp from './assets/icon_w.png'
 
-import Art from './Componentes/art'
+import Art from './Componentes/Art'
+
+import books from './assets/books.json'
 
 export default function App() {
   return (<>
@@ -18,12 +20,12 @@ export default function App() {
     de la pantalla y que estos no se modifiquen y mx-auto es para que 
     siempre tengan el mismo margen de los lados*/}
     <section className='grid md:grid-cols-3 gap-4 text-xl md:text-2xl container mx-auto'>
-       <Art num={0}></Art>
-       <Art num={1}></Art>
-       <Art num={2}></Art>
-       <Art num={3}></Art>
-       <Art num={4}></Art>
-       <Art num={5}></Art>
+       <Art name={books[0].name} genere={books[0].genere} image={books[0].image}/>
+       <Art name={books[1].name} genere={books[1].genere} image={books[1].image}/>
+       <Art name={books[2].name} genere={books[2].genere} image={books[2].image}/>
+       <Art name={books[3].name} genere={books[3].genere} image={books[3].image}/>
+       <Art name={books[4].name} genere={books[4].genere} image={books[4].image}/>
+       <Art name={books[5].name} genere={books[5].genere} image={books[5].image}/>
     </section>
 
     <footer className='w-full border-sky-500 border-t-2 p-2 flex text-small italic text-gray-600 justify-center mt-20'>
--- src/Componentes/Art.tsx
+++ src/Componentes/Art.tsx
@@ -1,12 +1,40 @@
-import { books } from "./books"
-export default function Art(props:{num:number}) {
-    return <>
-        <article className='flex gap-2 text-gray-400 hover:text-blue-600 rounded bg-stone-800 shadow hover:shadow-md hover:shadow-blue-700'>
-        <img className='w-20 h-28 object-cover m-2 border rounded' src={books[props.num].src} alt="" />
-        <hgroup className='flex flex-col justify-center'>
-          <h1>{books[props.num].name}</h1>
-          <p className='text-lg'>{books[props.num].genere}</p>
-        </hgroup>
-        </article>
-    </>
+// esto esta mal porque los tsx y jsx no son para poner funciones
+// ni para poner datos, son explicitamente para poner codigo de react
+// si no vas a crear codigo de rect usa solamente ts o js
+
+// ahora bien esto tampoco se importaria desde un js ni un ts ya que son solo datos
+// que posiblemente luego se cambien por bases de datos por lo tanto deveriamos crearlos
+// en un json de la siguiente forma (POSTDATA: Antes de hacer algo mas moveremos los 
+// archivos para la carpeta public porque ya no las usaremos con sus direciones relativas,
+// que las llamaremos desde la web como url)
+
+// Otra cosa que esta mal es que los componente no pueden acceder directamente a los datos, 
+// pues tienes que setearlos desde fuera por eso cambie las props
+// src lo cambie por image podria ser por cover o por tumbnails que son dos definiciones 
+// que aun no te eh explicado por eso dejemoslo como imagen, no lo dejamos com src, porque 
+// nos estamos refiriendo a la imagen del componente y estos tienen que ser reutilizables 
+// y src dignifica source, y si tu lo entenderas, pero imagina que alguien que mira tu codigo
+// no va al componente solo lo va a utilizar
+
+// import { books } from "./books"
+
+// Una recomendacion es cambiarle el nombre al componente porque primero
+// esto no es un articulo, es un libro, necesitas nombrarlo con un nombre
+// mas consistente, (Btn siempre se usa como Button, para no entrar en conflicto 
+// con la etiqueta button de html)
+
+// otra cosa este elemento <></> solo se usa cuando tu componente no tiene estructura 
+// de bloque, ejemplo cuando tu componente solo usa dos etiquetas separadas
+// <h1>Title</h1>
+// <p>descripcion</p>
+// q ue oasa con esto, que esto tiene que estar dentro de <></> porque son dos bloques 
+// en vez de uno pero en tu caso estas enviando un solo bloque el article, por eso podemos omitirlo
+export default function Art(props: { name: string, genere: string, image: string }) {
+  return <article className='flex gap-2 text-gray-400 hover:text-blue-600 rounded bg-stone-800 shadow hover:shadow-md hover:shadow-blue-700'>
+    <img className='w-20 h-28 object-cover m-2 border rounded' src={props.image} alt="" />
+    <hgroup className='flex flex-col justify-center'>
+      <h1>{props.name}</h1>
+      <p className='text-lg'>{props.genere}</p>
+    </hgroup>
+  </article>
 }
