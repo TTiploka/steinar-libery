7e4c502d9508a4aa6a70b269fa6a6eac0165470a
TTiploka <hcobasmatos@gmail.com>
2023-10-16 18:26:40 -0400

    Six Roots Added

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomeView from "../views/HomeView";
+import About from "../views/about";
 
 const router = createBrowserRouter([
     { path: "/", element: <HomeView/> },
+    { path: "/about", element: <About/> },
 ]);
 
 export default router
