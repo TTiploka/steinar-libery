d45fcec0b474ed0c2480940e19d5324e5e78db95
TTiploka <hcobasmatos@gmail.com>
2023-10-24 16:12:14 -0400

    feature/bugsRemoved

--- src/views/ContactUs.tsx
+++ src/views/ContactUs.tsx
@@ -1,15 +1,16 @@
+// le puse un hover para que cambie de color a ver cual te gustaba mas
 export default function ContactUs() {
     return <>
-        <article className="border text-white text-left flex gap-0 4/5 sm:w-3/5 h-auto p-2 my-20 mx-auto">
-            <div className="bg-gray-900 rounded-l-md flex flex-col gap-5 marker:items-start w-auto m-10 sm:4/6 md:w-4/5 md:m-auto lg:w-1/2 py-5 px-5">
-                <h1    className="text-white    text-center self-center text-3xl">Contact us</h1>
+        <article className=" text-white text-left flex gap-0 4/5 sm:w-3/5 h-auto p-2 my-20 mx-auto">
+            <div className="bg-gradient-to-r from-gray-900 hover:to-gray-800   rounded-l-lg flex flex-col gap-5 marker:items-start w-auto m-10 sm:4/6 md:w-4/5 md:m-auto lg:w-1/2 py-5 px-5">
+                <h1    className="text-white text-center self-center text-3xl">Contact us</h1>
                 <small className="text-gray-400 text-center self-center -mt-5 mb-5">Tell us what do you think</small>
-                <input    className="rounded-sm text-black text-xl w-full p-2" type="text" name="user"  placeholder="Name" />
-                <input    className="rounded-sm text-black text-xl w-full p-2" type="text" name="mail" placeholder="Mail Address" />
-                <textarea className="rounded-sm text-black text-xl w-full p-2" name="comentary" rows={4} cols={50} placeholder="Write your message here" />
-                <button className="bg-indigo-500 rounded-md self-center hover:shadow-md hover:shadow-indigo-800 w-1/2 h-10 my-5">Submit</button>
+                <input    className="rounded-md text-black text-xl w-full p-2" type="text" name="user"  placeholder="Name" />
+                <input    className="rounded-md text-black text-xl w-full p-2" type="text" name="mail" placeholder="Mail Address" />
+                <textarea className="rounded-md text-black text-xl w-full p-2" name="comentary" rows={4} cols={50} placeholder="Write your message here" />
+                <button className="bg-indigo-500 rounded-md self-center hover:shadow-md hover:shadow-indigo-800 w-full h-10 my-5">Submit</button>
             </div>
-            <img className="rounded-r-md w-1/2 h-auto hidden m-0 lg:block" src="src/assets/fondo.jpg" alt="src/assets/nofoto.png" />
+            <img className="rounded-r-lg w-1/2 h-auto hidden m-0 lg:block" src="src/assets/abovt.jpg" alt="src/assets/nofoto.png" />
         </article>
     </>
 }
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -2,8 +2,7 @@ export default function Login() {
     return <>
     <article className="text-white flex text-left shadow-md shadow-stone-900 w-4/5 sm:w-3/5 h-auto my-20 mx-auto">
         <div className="bg-stone-900 rounded-l-md flex flex-col gap-3 w-full md:w-1/2 py-5 px-5">
-            <h1 className="text-white text-center text-3xl">Login</h1>
-            <small className="block text-gray-500 text-center mb-5">Type your data to login</small>
+            <h1 className="text-white text-center text-3xl mb-5">Login</h1>
             <input className="rounded-md text-black text-xl w-full p-2 mb-5" type="text" placeholder="Email" />
             <input className="rounded-md text-black text-xl w-full p-2 " type="password" placeholder="Password" />
             <button className="bg-indigo-500 rounded-md self-center w-1/2 h-10 my-5 hover:shadow-md hover:shadow-indigo-800">Submit</button>
